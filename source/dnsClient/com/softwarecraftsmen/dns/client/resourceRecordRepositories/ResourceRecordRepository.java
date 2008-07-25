58051558507ecf6fbc801d147fc1cd804c3182a1
raphael.james.cohn <raphael.james.cohn@dbd9cc12-c152-0410-9215-c5c0cfec61ea>
2008-07-25 09:18:05 +0000

    Preparing the ground for caching

---
+++ source/dnsClient/com/softwarecraftsmen/dns/client/resourceRecordRepositories/ResourceRecordRepository.java
@@ -0,0 +1,17 @@
+/**
+ * This file is Copyright © 2008 Software Craftsmen Limited. All Rights Reserved.
+ */
+package com.softwarecraftsmen.dns.client.resourceRecordRepositories;
+
+import com.softwarecraftsmen.dns.Name;
+import com.softwarecraftsmen.dns.messaging.InternetClassType;
+import com.softwarecraftsmen.dns.messaging.serializer.Serializable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public interface ResourceRecordRepository
+{
+	@NotNull
+	<T extends Serializable> Set<T> findData(final @NotNull Name name, final @NotNull InternetClassType internetClassType);
+}
